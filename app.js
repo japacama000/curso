8d57067e6bdabad0ad47ccdae9360c606795ab9f
japacama000 <koike.luiz@escola.pr.gov.br>
2025-10-10 11:50:39 -0300

    salve1

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-h.hhhhhh
+let titulo = document.querrySelector('h1);');
+titulo.innerHTML = 'jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'escolha um número entre 1 e 10';
+
+function exibirTextoNaTela() {
+    let camplo = document.querySelector(tag);
+    camplo.inneHTML = texto;
+}
+
+exibirTextoNaTela('h1')
+exibirTextoNaTela('p')
+
+function verificarChute() {
+    
+}
